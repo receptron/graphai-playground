f4c357fa72ba1354df4609a55b1d0e4fb8248dd3
isamu <isamu@to-kyo.to>
2024-06-20 11:36:47 +0900

    graphai_express_type

--- src/views/home/utils.ts
+++ src/views/home/utils.ts
@@ -1,10 +1,11 @@
 import { ref, computed } from "vue";
 import YAML from "yaml";
 
-import { AgentFunctionContext, AgentFunctionInfoSample } from "graphai";
+import { AgentFunctionContext } from "graphai";
 
 import { graphDataSet } from "@/utils/graph_data";
 import { streamAgentFilterGenerator, httpAgentFilter } from "@graphai/agent_filters";
+import { ExpressAgentInfo } from "@receptron/graphai_express_type";
 
 export const useSelectGraph = () => {
   const selectedGraphIndex = ref(0);
@@ -66,22 +67,6 @@ export const getAgentFilter = <T>(
     },
   ];
   return agentFilters;
-};
-
-// TODO: from express
-type ExpressAgentInfo = {
-  agentId: string;
-  name: string;
-  url: string;
-  description: string;
-  category: string[];
-  author: string;
-  license: string;
-  repository: string;
-  samples: AgentFunctionInfoSample[];
-  inputs: any;
-  output: any;
-  stream: boolean;
 };
 
 export const useServerAgent = (agentBaseUrls: string[]) => {
